14712a0229a4ec049870772f40d46ebc5317b71c
bartmnz <bartmnz@gmail.com>
2016-03-18 16:00:43 +0000

    fixed error handeling

--- if.py
+++ if.py
@@ -8,10 +8,10 @@ class Place(object):
         self.finished_places = finished_places
         self.in_thing = []
     def light(self, item):
-        print('no fire for ugg')
+        print('no ' + str(item[0]) + ' for ugg')
         return self
     def examine(self, item):
-        print (' you look closely at the ' + str(*item) + ' and see nothing useful')
+        print (' you look closely at the ' + str(item[0]) + ' and see nothing useful')
         #TODO -- #implement
         return self
     def get_take(self, item):
@@ -21,6 +21,11 @@ class Place(object):
                 self.items.append('prism')
                 self.items.append('pickle')
                 self.finished_places += 1
+        if (self.items):
+            self.items.append(str(*item))
+        else:
+            self.items = [str(*item)]
+        print (self.items)
         return self
     def openn( self, thing):
         if thing[0] == 'door':
@@ -29,16 +34,24 @@ class Place(object):
         return self
     def drop(self, item):
         item = item[0]
-        if item not in self.items:
+        print (item)
+        if not (item in self.items):
             print( "you don't have a " + str(item) + " to drop")
         self.items.remove(item)
         return self
         #implement
     def put_in(self, items):
-        if items[0] not in self.items:
-            print( "you don't have a " + str(items[0]))
-        if items[1] not in self.items:
-            print( "you don't have a " + str(items[1]))
+        try:
+            if items[0] not in self.items:
+                print( "you don't have a " + str(items[0]))
+                return self
+            if items[2] not in self.items:
+                print( "you don't have a " + str(items[1]))
+                return self
+        except IndexError:
+            print ( 'put ' + str(items[0]) + ' where')
+        except TypeError:
+            print ('you don\'t have anything')
         return self
         #implement
         
@@ -47,22 +60,26 @@ class Place(object):
         return self
     def move(self, direction):
         #direction = direction[0]
-        print ('moving ' + direction)
-        if self.in_thing:
-            print( "You have to get out of the " + str(*self.in_thing[-1]) +" first" )
-            return self
-        if direction == 'north':
-            if self.finished_places == 12:
-                self.finished_places += 1
-            return North(self.items, self.finished_places)
-        if direction == 'up':
-            if self.finished_places == 4:
-                self.finished_places += 1
-            return Up(self.items, self.finished_places)
-        if direction == 'east':
-            if self.finished_places == 2:
-                self.finished_places += 1
-            return East(self.items, self.finished_places)
+        try:
+            
+            if self.in_thing:
+                print( "You have to get out of the " + str(*self.in_thing[-1]) +" first" )
+                return self
+            if direction == 'north':
+                if self.finished_places == 12:
+                    self.finished_places += 1
+                return North(self.items, self.finished_places)
+            if direction == 'up':
+                if self.finished_places == 4:
+                    self.finished_places += 1
+                return Up(self.items, self.finished_places)
+            if direction == 'east':
+                if self.finished_places == 2:
+                    self.finished_places += 1
+                return East(self.items, self.finished_places)
+        except AttributeError:
+            self.items = []
+            self.move(direction)
         return Place(self.items, self.finished_places)
         
         #implement
@@ -109,15 +126,17 @@ class Up(Place):
 # PUT PRISM IN PICKLE
 # EXIT CAVE
     def __init__(self, items, finished_places):
-        print ('makeing up')
         super(Up, self).__init__(items, finished_places)
         self.items.append('helmet')
     def light(self, item):
         print ('ohh fire')
-        if self.finished_places == 6:
-            if item[0] == 'fire':
-                self.items.append('fire')
+        
+        
+        if item[0] == 'fire':
+            self.items.append('fire')
+            if self.finished_places == 6:
                 self.finished_places += 1
+        
         return self
         #if item is fire do stuff
     def put_in(self, item):
@@ -126,7 +145,10 @@ class Up(Place):
             action = item[1]
             item = item[0]
         except IndexError:
+            print ( 'put ' + str(item[0]) + ' where')
             return self
+        except TypeError:
+            print ('you don\'t have anything')
         if place not in self.items:
             print( "you don't have a " + str(place))
         elif item not in self.items:
@@ -152,12 +174,14 @@ class Up(Place):
             self.finished_places += 1
         return self
     def enter(self, thing):
+        super(Up, self).enter(thing)
         if thing[0] == 'cave':
             if self.finished_places == 5:
                 self.items.append('statue')
                 self.finished_places += 1
         return self
     def exit(self, thing):
+        super(Up, self).exit(thing)
         if thing[0] == 'cave':
             if self.finished_places == 11:
                 self.items.remove('statue')
@@ -195,6 +219,13 @@ def main():
             quest = todo[user[0]](user[1::])
         except KeyError:
             quest = quest.move(user[0])
+        except IndexError:
+            if( user ):
+                print (str(user[0]) + ' what?')
+            else:
+                print ("Enter a command!")
+        except ValueError:
+            print ('you don\'t have anything')
             
         # input()
         # do task
